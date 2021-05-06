c1e33f55ab7faac5cd1582f72e8f288aefd2f168
SauhardoSengupta <70318077+sauhardo2020@users.noreply.github.com>
2021-05-06 17:57:55 +0530

    Update main.py

--- Mean,Median,Mode/main.py
+++ Mean,Median,Mode/main.py
@@ -3,7 +3,7 @@ import csv
 # Reading File Data
 CSV_File_OBJ = open('Mean,Median,Mode\SOCR-HeightWeight.csv')
 Reader = csv.reader(CSV_File_OBJ)
-  
+
 # Deleting The Title ans storing it in a list
 All_CSV_Data_Title = list(Reader)
 All_CSV_Data_Title.pop(0)
@@ -11,9 +11,9 @@ All_CSV_Data_Title.pop(0)
 # Creating a List to later store all the height Data
 All_Weight = []
 
-#Finding/Getting the weight inside a list
+# Finding/Getting the weight inside a list
 for i in range(len(All_CSV_Data_Title)):
-
+   
     # Getting The Weight and stoaring it in All_Weights
     Weights = All_CSV_Data_Title[i][1]
     All_Weight.append(float(Weights))
@@ -22,10 +22,10 @@ Total = 0
 
 # Adding(Sum)
 for x in All_Weight:
-    Total = Total + float(x)
+    Total += float(x)
 
 # Length of Weights...
 Length_Of_Weights = len(All_Weight)
-Mean = Total % Length_Of_Weights
+Mean = Total / Length_Of_Weights
 
 print('Median(Average)' + '-> ' + str(Mean))
